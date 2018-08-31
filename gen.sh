3a68f0a30f55aa052dd8b4d1f3c075984cd6e082
VelocityRa <makren67@gmail.com>
2018-09-01 01:26:44 +0300

    build: Set actual boost build dir and clean it before building
    - Previously I was only setting stage dir, resulting in dirty submodule repo
      after running gen.bat
    - Also fix a path issue in Windows

--- gen.sh
+++ gen.sh
@@ -4,14 +4,16 @@ set -ex
 # CI uses pre-built Boost
 if [[ -z "${CI}" ]]; then
 	# Create build dir
+	rm -rf src/external/boost-build
 	mkdir -p src/external/boost-build
 	cd src/external/boost
 
 	# Non-Windows needs to build Boost.Build first
 	chmod +x tools/build/src/engine/build.sh
 	sh bootstrap.sh
-	./b2 -j5 --stagedir=../boost-build stage
-	
+
+	# Build our Boost subset
+	./b2 -j5 --build-dir=../boost-build --stagedir=../boost-build stage
 	cd ../../..
 fi
 
